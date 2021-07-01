cb7b1f62b3772dacd7908016abc501e99ecbd53a
If_0110 <k019g1201@g.neec.ac.jp>
2021-07-01 14:21:56 +0900

    Enemy2挙動

--- Janp/Enemy2.cpp
+++ Janp/Enemy2.cpp
@@ -7,15 +7,18 @@ Enemy2::Enemy2()
 	spawn = false;
 	die = false;
 	move = false;
-	attack = Count();
-
-	arm = GameObject("Enemy2", false, Vector2(32, 64));
+	attack_Time = Count();
+	attack_End_Time = Count();
+	rush_Time = Count();
+	attack_front_flame = false;
+	arm = GameObject("Enemy2", false, Vector2(32, 128));
 	arm.color = COLOR(255, 0, 0);
-
-	body = GameObject("Enemy2", false, Vector2(120, 192));
+	attack_Animetion_flg = 0;
+	body = GameObject("Enemy2", false, Vector2(120, 160));
 	body.color = COLOR(255, 150, 50);
 	ani = Animation();
-
+	uturn[0] = false;
+	uturn[1] = false;
 }
 
 Enemy2::~Enemy2()
@@ -37,10 +40,10 @@ void Enemy2::Init(std::vector<std::vector<int>>& collMap)
 					spawn = true;
 					game_object.game.allVec.pos = Vector2(SIZE * x, SIZE * y);
 					game_object.game.dis = true;
-					attack = Count();
+					attack_Time = Count();
 
 					arm.game.dis = false;
-					arm.SetPos(Vector2(game_object.GetPos().x - 64, game_object.GetPos().y + 32));
+					arm.SetPos(Vector2(game_object.GetPos().x - 32, game_object.GetPos().y + 32));
 
 					body.game.dis = true;
 					body.SetPos(Vector2(game_object.GetPos().x + 32, game_object.GetPos().y));
@@ -64,45 +67,84 @@ void Enemy2::Update(const Vector2& pos, Collision* coll)
 	{
 		if (!die)
 		{
+			float max_speed = 0.0f;
+			float flame_speed = 0.0f;
 			bool lr = game_object.game.lr;
-			AllUpdate(0.2f, 4.6f);
-			PlayerCahck(pos, coll);
+			attack_front_flame = attack_Time.flg;
+
 			body.game.lr = lr;
 			arm.game.lr = lr;
+
+			max_speed = 4.8f;
+			flame_speed = 0.2f;
 
 			if (lr)body.SetPos(Vector2(game_object.GetPos().x + 64, game_object.GetPos().y));
 
 			else body.SetPos(Vector2(game_object.GetPos().x + 64, game_object.GetPos().y));
 
-			if (attack.flg)
+			if (attack_Time.Conuter(30))
+			{
+				attack_Animetion_flg = 1;
+				attack_Time = Count();
+			}
+			if (attack_Animetion_flg == 1)
 			{
 				arm.game.dis = true;
 				tex = attackTex[attackAni.num];
-				int attackAniNum[7] = { 3,3,3,3,30,2,2 };
+				int attackAniNum[7] = { 3,3,3,3,30,5,7 };
 				if (attackAni.OneAnimation(attackAniNum[attackAni.num], 7))
 				{
 					attackAni.num = 0;
-					attack.flg = false;
+					attack_Animetion_flg = 2;
+					arm.game.dis = false;
+					tex = enemy2Tex[0];
 				}
-				if (lr)
-				{
 
-				}
-				else
-				{
-
-				}
-				arm.game.allVec.AddPos();
 			}
-			else
+			else if (attack_Animetion_flg == 0)
 			{
 				arm.game.dis = false;
 				tex = enemy2Tex[ani.num];
 				attackAni = Animation();
 				ani.AnimationOn(4, 21);
-				if (lr)arm.SetPos(Vector2(game_object.GetPos().x - 64, game_object.GetPos().y + 32));
-				else arm.SetPos(Vector2(game_object.GetPos().x + 64, game_object.GetPos().y + 32));
+				if (lr)arm.SetPos(Vector2(game_object.GetPos().x + 32, game_object.GetPos().y + 64));
+				else arm.SetPos(Vector2(game_object.GetPos().x + game_object.game.size.x - 32, game_object.GetPos().y + 64));
 			}
+			if (attack_Animetion_flg == 2)
+			{
+				attack_End_Time.flg = true;
+				if (attack_End_Time.Conuter(10))
+				{
+					attack_Animetion_flg = 3;
+					if (true)
+					{
+						attack_Animetion_flg = 4;
+						rush_Time.flg = true;
+					}
+				}
+			}
+			if (attack_Animetion_flg == 4)
+			{
+				tex = enemy2Tex[ani.num];
+				ani.AnimationOn(3, 21);
+
+				max_speed = 10.8f;
+				flame_speed = 0.2f;
+
+
+				if (lr)game_object.game.allVec.vec.x -= 6.0f;
+				else game_object.game.allVec.vec.x += 6.0f;
+
+				if (uturn[0])lr = false;
+				if (uturn[1])lr = true;
+				if (rush_Time.Conuter(180))
+				{
+					attack_Animetion_flg = 3;
+				}
+			}
+
+			AllUpdate(flame_speed, max_speed);
+			PlayerCahck(pos, coll);
 		}
 	}
 }
@@ -110,42 +152,59 @@ void Enemy2::Update(const Vector2& pos, Collision* coll)
 void Enemy2::PlayerCahck(const Vector2& pos, Collision* coll)
 {
 	Vector2 layer = game_object.GetPos();
-	const int LAYERSIZE = 128 * 4;
-	const int ATAACKSIZE = 32;
+	const int LAYERSIZE = 128 * 10;
+	const int ATAACKSIZE = 16;
 	float distanceL = Vector2::Distance(Vector2(pos.x + SIZE * 2, pos.y), game_object.GetPos());
 	float distanceR = Vector2::Distance(pos, Vector2(game_object.GetPos().x + game_object.game.size.x, game_object.GetPos().y));
-
-	if (coll->Collsion(layer, ATAACKSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
+	if (attack_Animetion_flg != 1)
 	{
-		attack.flg = true;
-		game_object.game.lr = true;
-		ani.num = 0;
-	}
-	else if (coll->Collsion(layer, ATAACKSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
-	{
-		attack.flg = true;
-		game_object.game.lr = false;
-		ani.num = 0;
-	}
-	else
-	{
-		attack = Count();
-		//右
-
-		if (coll->Collsion(layer, LAYERSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
+		if (coll->Collsion(Vector2(layer.x - ATAACKSIZE, layer.y), ATAACKSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
 		{
-			game_object.game.allVec.vec.x += 0.4f;
-			game_object.game.lr = false;
+			//左
+			if (attack_Animetion_flg == 0)
+			{
+				attack_Time.flg = true;
+				game_object.game.lr = true;
+				ani.num = 0;
+			}
 		}
-
-
-		//左
-
-		layer.x -= LAYERSIZE;
-		if (coll->Collsion(layer, LAYERSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
+		else if (coll->Collsion(Vector2(layer.x + game_object.game.size.x, layer.y), ATAACKSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
 		{
-			game_object.game.allVec.vec.x -= 0.4f;
-			game_object.game.lr = true;
+			//右
+			if (attack_Animetion_flg == 0)
+			{
+				attack_Time.flg = true;
+				game_object.game.lr = false;
+				ani.num = 0;
+			}
+
+		}
+		else
+		{
+			if (attack_Animetion_flg == 0 ||attack_Animetion_flg == 3)
+			{
+
+				//右
+
+				if (coll->Collsion(Vector2(layer.x + game_object.game.size.x, layer.y), LAYERSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
+				{
+					game_object.game.allVec.vec.x += 0.4f;
+					game_object.game.lr = false;
+					attack_Animetion_flg = 0;
+				}
+
+
+				//左
+
+				layer.x -= LAYERSIZE;
+				if (coll->Collsion(layer, LAYERSIZE, game_object.game.size.y, pos, SIZE * 2, SIZE * 2))
+				{
+					game_object.game.allVec.vec.x -= 0.4f;
+					game_object.game.lr = true;
+					attack_Animetion_flg = 0;
+				}
+
+			}
 		}
 	}
 
@@ -156,7 +215,81 @@ void Enemy2::MapCollUpdate(std::vector<std::vector<int>>& collMap)
 	Map_Coll(collMap);
 }
 
+void Enemy2::Map_Coll(std::vector<std::vector<int>>& collMap)
+{
+	//上左
+	MapJub(MapPointerY(1, 1, collMap), 0);
+	//上右
+	MapJub(MapPointerY(body.game.size.x - 1, 1, collMap), 0);
 
+	//下左
+	MapJub(MapPointerY(1, body.game.size.y - 1, collMap), 1);
+	//下右
+	MapJub(MapPointerY(body.game.size.x - 1, body.game.size.y - 1, collMap), 1);
+
+	game_object.game.allVec.AddPosY();
+
+	//右上
+	MapJub(MapPointerX(body.game.size.x - 1, 1, collMap), 2);
+	//右下
+	MapJub(MapPointerX(body.game.size.x - 1, body.game.size.y - 1, collMap), 2);
+
+	//左上
+	MapJub(MapPointerX(1, 1, collMap), 3);
+	//左下
+	MapJub(MapPointerX(1, body.game.size.y - 1, collMap), 3);
+
+	game_object.game.allVec.AddPosX();
+}
+
+
+void Enemy2::MapJub(const int& mapPoint, const int& pointNum)
+{
+	if (pointNum == 0) //Y軸
+	{
+		if (WALL)
+		{
+			game_object.game.allVec.vec.y -= game_object.game.allVec.vec.y;
+			game_object.game.allVec.vec.y = 0;
+		}
+	}
+	else if (pointNum == 1) //Y軸
+	{
+		if (WALL)
+		{
+			game_object.game.allVec.vec.y -= game_object.game.allVec.vec.y;
+			game_object.game.allVec.vec.y = 0;
+		}
+	}
+
+	else if (pointNum == 2)//X軸
+	{
+		if (WALL)
+		{
+			game_object.game.allVec.vec.x -= game_object.game.allVec.vec.x;
+			game_object.game.allVec.vec.x = 0;
+			uturn[0] = true;
+		}
+		else
+		{
+			uturn[0] = false;
+		}
+	}
+
+	else if (pointNum == 3)//X軸
+	{
+		if (WALL)
+		{
+			game_object.game.allVec.vec.x -= game_object.game.allVec.vec.x;
+			game_object.game.allVec.vec.x = 0;
+			uturn[1] = true;
+		}
+		else
+		{
+			uturn[1] = false;
+		}
+	}
+}
 
 void Enemy2::Coll(std::vector<Explosion>& ex)
 {
--- Janp/Enemy2.h
+++ Janp/Enemy2.h
@@ -20,13 +20,22 @@ public:
 	GameObject arm;
 	GameObject body;
 private:
+
+	void Map_Coll(std::vector<std::vector<int>>& collMap);
+	void MapJub(const int& mapPoint, const int& pointNum);
+
 	int tex;
 	int  enemy2Tex[21];
 	int attackTex[7];
 	bool spawn;
 	bool die;
 	bool move;
-	Count attack;
+	bool uturn[2];
+	Count attack_Time;//近づいてから攻撃までの時間
+	Count attack_End_Time;//攻撃が終わってからのクールタイム
+	Count rush_Time;//突進時間
+	int attack_Animetion_flg;//攻撃を始める
+	bool attack_front_flame;//前フレームAttackフラグがOnだったか
 	Animation ani;
 	Animation attackAni;
 };
